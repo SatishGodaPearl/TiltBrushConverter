4318c932df3b1663732c5216327fcc7dd07f2b3f
Satish Goda <satishgoda@pearlstudio.com>
2018-04-05 18:58:09 -0700

    Updated paths after installing the prerequisites
    https://github.com/DrHibbitts/TiltBrushConverter/blob/master/README.md

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-tiltBrushInstallDir = r"D:\SteamLibrary\SteamApps\common\Tilt Brush"
-FBXSDKInstallDir = r'D:\Program Files\Autodesk\FBX\FBX Python SDK\2015.1\lib\Python27_x64'
-tiltBrushExportDir = r"D:\Documents\Tilt Brush\Exports"
+tiltBrushInstallDir = r"C:\Program Files (x86)\Steam\steamapps\common\Tilt Brush"
+FBXSDKInstallDir = r'C:\Program Files\Autodesk\FBX\FBX Python SDK\2015.1\lib\Python27_x64'
+tiltBrushExportDir = r"C:\Users\satish\Documents\TiltBrush"
